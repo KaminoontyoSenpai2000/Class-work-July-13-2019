f890ca229c413e1721972071a976c0455d574c7f
Ivan <49728539+KaminoontyoSenpai2000@users.noreply.github.com>
2019-07-13 21:01:49 +0300

    Add files via upload

---
+++ Class work July 13 2019/1/main.js
@@ -0,0 +1,48 @@
+/*var modal = document.querySelector('.modal');
+
+function toggleModal() {
+		if (modal.classList.contains('visible')) {
+			modal.classList.remove('visible');	
+		} else{
+			modal.classList.add('vivsible')
+		}
+	}
+
+var clickMeButton = document.getElementById('clickme');
+var clickMeButton = document.getElementById('close');
+
+clickMeButton.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+
+
+Это было заданием первым
+*/
+
+
+/*var i = 0;
+while(i < 10) {
+	alert(i);
+	i++;
+}
+*/
+
+
+
+/*for(var i = 0; i <100; i++){
+	if ( i % 3 === 0 && i % 5 === 0) {
+		console.log('кратно 3 и кратно 5');
+	}
+
+	else if (i % 3 ===0){
+		console.log('кратно 3');
+	}
+
+	else if (i % 5 ===0){
+		console.log('кратно 5');
+	}
+
+	else {
+		console.log(i);
+	}
+
+}*/
